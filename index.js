48abfa2e64c6b9b0c7917176f7aa6cf7a8f39cfa
Sahana-hurali <83111561+Sahana-hurali@users.noreply.github.com>
2021-05-28 19:25:56 +0530

    Content of Blog Page

---
+++ index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./Content.css";
+
+const Content = () => {
+    return (  
+        <div className="textContent1">
+            <div id="content1">
+            <h2>Updating our Rules</h2>
+            <p>Medium is an open platform that exists to share ideas and perspectives from the world’s most insightful writers, thinkers, and storytellers. <br /><br />
+Today, we are updating our Rules. As Medium evolves as a product, so do the ways people use it. To accommodate this, we regularly assess our rules, and adjust them accordingly.<br /><br />
+Changes today include strengthening our rules regarding content that violates the Terms of Service of Medium or other third parties, and our policies on copyright.<br /><br />
+<hr/></p>
+ </div>
+ <div id="content2">
+            <h2>Improving Discovery on Medium with Tags</h2>
+           <h3>Check out the new tag pages</h3>
+            <p>Recently we’ve launched some updates to the tag pages on the web to improve discovery. <br /><br />
+            Tags are keywords and keyword phrases that are added to stories by the writer to improve story discovery and categorization. A writer can add up to five tags per story.<br /><br />
+            
+</p>
+{/* <img src="https://miro.medium.com/max/700/1*0sCjtyxho_7ZzttkXGTAkg.png" className="image" title="Tags in the post editor"></img> */}
+
+ </div>
+            
+        </div>
+        
+    );
+}
+ 
+export default Content;

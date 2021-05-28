b0930fa08e517ecc41e7bc3a69eb4cadeced6bb0
Sahana-hurali <83111561+Sahana-hurali@users.noreply.github.com>
2021-05-28 19:27:21 +0530

    Blog's Top Bar

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 import React from 'react';
-import "./Content.css";
+import "./TopBar.css";
 
-const Content = () => {
-    return (  
-        <div className="textContent1">
-            <div id="content1">
-            <h2>Updating our Rules</h2>
-            <p>Medium is an open platform that exists to share ideas and perspectives from the world’s most insightful writers, thinkers, and storytellers. <br /><br />
-Today, we are updating our Rules. As Medium evolves as a product, so do the ways people use it. To accommodate this, we regularly assess our rules, and adjust them accordingly.<br /><br />
-Changes today include strengthening our rules regarding content that violates the Terms of Service of Medium or other third parties, and our policies on copyright.<br /><br />
-<hr/></p>
- </div>
- <div id="content2">
-            <h2>Improving Discovery on Medium with Tags</h2>
-           <h3>Check out the new tag pages</h3>
-            <p>Recently we’ve launched some updates to the tag pages on the web to improve discovery. <br /><br />
-            Tags are keywords and keyword phrases that are added to stories by the writer to improve story discovery and categorization. A writer can add up to five tags per story.<br /><br />
-            
-</p>
-{/* <img src="https://miro.medium.com/max/700/1*0sCjtyxho_7ZzttkXGTAkg.png" className="image" title="Tags in the post editor"></img> */}
-
- </div>
-            
-        </div>
-        
-    );
+const TopBar = () => {
+    return ( 
+    <div className="TopBar">
+     
+        <h1 className="h1">3 Min Read</h1>
+        <h4 className="h4">The official Medium blog</h4>
+    </div> );
 }
  
-export default Content;
+export default TopBar;
